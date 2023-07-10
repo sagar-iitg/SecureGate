d0054d434261d8ad97a481bc6fc296b5e96ea945
sagar <sagar-iitg>
2023-07-10 17:25:32 +0530

    hackathon initial commit

---
+++ src/main/java/com/hclsw/SecuregateApplication.java
@@ -0,0 +1,13 @@
+package com.hclsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuregateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuregateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hclsw/SecuregateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hclsw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuregateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
