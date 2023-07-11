e75b1b1c80bb16d1f54928ac0fafd4f7e1725d84
sagar <sagar-iitg>
2023-07-11 13:28:33 +0530

    Adding home controller

---
+++ src/main/java/com/hclsw/controllers/HomeController.java
@@ -0,0 +1,22 @@
+package com.hclsw.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+
+    @GetMapping
+    public String testing() {
+
+        return "Welcome to HCL Software Hackathon";
+
+    }
+
+}
+
+
